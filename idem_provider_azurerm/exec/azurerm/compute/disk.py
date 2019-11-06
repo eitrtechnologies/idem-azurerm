7088db64e6a4d74d5e7a57481837f56b185f08f7
nicholasmhughes <nicholasmhughes@gmail.com>
2019-11-05 22:48:46 -0500

    initial stub of disk exec module

---
+++ idem_provider_azurerm/exec/azurerm/compute/disk.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+'''
+Azure Resource Manager (ARM) Compute Disk Execution Module
+
+.. versionadded:: 1.0.0
+
+:maintainer: <devops@eitr.tech>
+:maturity: new
+:depends:
+    * `azure <https://pypi.python.org/pypi/azure>`_ >= 4.0.0
+    * `azure-common <https://pypi.python.org/pypi/azure-common>`_ >= 1.1.23
+    * `azure-mgmt <https://pypi.python.org/pypi/azure-mgmt>`_ >= 4.0.0
+    * `azure-mgmt-compute <https://pypi.python.org/pypi/azure-mgmt-compute>`_ >= 4.6.2
+    * `azure-mgmt-network <https://pypi.python.org/pypi/azure-mgmt-network>`_ >= 4.0.0
+    * `azure-mgmt-resource <https://pypi.python.org/pypi/azure-mgmt-resource>`_ >= 2.2.0
+    * `azure-mgmt-storage <https://pypi.python.org/pypi/azure-mgmt-storage>`_ >= 2.0.0
+    * `azure-mgmt-web <https://pypi.python.org/pypi/azure-mgmt-web>`_ >= 0.35.0
+    * `azure-storage <https://pypi.python.org/pypi/azure-storage>`_ >= 0.36.0
+    * `msrestazure <https://pypi.python.org/pypi/msrestazure>`_ >= 0.6.1
+:platform: linux
+
+:configuration: This module requires Azure Resource Manager credentials to be passed as keyword arguments
+    to every function in order to work properly.
+
+    Required provider parameters:
+
+    if using username and password:
+      * ``subscription_id``
+      * ``username``
+      * ``password``
+
+    if using a service principal:
+      * ``subscription_id``
+      * ``tenant``
+      * ``client_id``
+      * ``secret``
+
+    Optional provider parameters:
+
+**cloud_environment**: Used to point the cloud driver to different API endpoints, such as Azure GovCloud.
+    Possible values:
+      * ``AZURE_PUBLIC_CLOUD`` (default)
+      * ``AZURE_CHINA_CLOUD``
+      * ``AZURE_US_GOV_CLOUD``
+      * ``AZURE_GERMAN_CLOUD``
+
+'''
+
+# Python libs
+from __future__ import absolute_import
+import logging
+
+# Azure libs
+HAS_LIBS = False
+try:
+    import azure.mgmt.compute.models  # pylint: disable=unused-import
+    from msrest.exceptions import SerializationError
+    from msrestazure.azure_exceptions import CloudError
+    from msrestazure.tools import is_valid_resource_id
+    HAS_LIBS = True
+except ImportError:
+    pass
+
+log = logging.getLogger(__name__)
+
+
+async def delete(hub, name, resource_group, **kwargs):
+    '''
+    .. versionadded:: 1.0.0
+
+    Delete a disk.
+
+    :param name: The disk to delete.
+
+    :param resource_group: The resource group name assigned to the disk.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        azurerm.compute.disk.delete testdisk testgroup
+
+    '''
+    result = False
+    compconn = await hub.exec.utils.azurerm.get_client('compute', **kwargs)
+    try:
+        compconn.disks.delete(
+            resource_group_name=resource_group,
+            disk_name=name
+        )
+        result = True
+
+    except CloudError as exc:
+        await hub.exec.utils.azurerm.log_cloud_error('compute', str(exc), **kwargs)
+
+    return result
