47351e558f193663964e1cb20246357093592891
nicholasmhughes <nicholasmhughes@gmail.com>
2020-03-26 15:23:28 -0400

    handle case where tags key is present but contains None

--- idem_provider_azurerm/states/azurerm/keyvault/key.py
+++ idem_provider_azurerm/states/azurerm/keyvault/key.py
@@ -146,7 +146,10 @@ async def present(hub, ctx, name, key_type, vault_url, key_ops=None, enabled=Non
 
     if 'error' not in key:
         if tags:
-            tag_changes = await hub.exec.utils.dictdiffer.deep_diff(key.get('properties', {}).get('tags', {}), tags or {})
+            tag_changes = await hub.exec.utils.dictdiffer.deep_diff(
+                key.get('properties', {}).get('tags', {}) or {},
+                tags or {}
+            )
             if tag_changes:
                 ret['changes']['tags'] = tag_changes
 
