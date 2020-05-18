01eed9785d693c8f1030818efad2ee507db30bc5
nicholasmhughes <nicholasmhughes@gmail.com>
2020-05-17 21:26:43 -0400

    tweaking integration test stub

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,6 +5,9 @@ import pytest
 @pytest.fixture(scope="session")
 def hub():
     hub = pop.hub.Hub()
-    for dyne in ("exec", "states"):
-        hub.pop.sub.add(dyne_name=dyne)
+    hub.pop.sub.add(dyne_name="acct")
+    hub.pop.sub.add(dyne_name="exec")
+    hub.pop.sub.load_subdirs(hub.exec, recurse=True)
+    hub.pop.sub.add(dyne_name="states")
+    hub.pop.sub.load_subdirs(hub.states, recurse=True)
     return hub
--- tests/integration/states/azurearm/resource/test_group.py
+++ tests/integration/states/azurearm/resource/test_group.py
@@ -1,38 +1,15 @@
 import pytest
 
 
-# Option 1
-@pytest.fixture
-def hub(hub):
-    # Setup
-    ret = await hub.states.azurerm.resource.group.absent("name")
-    assert ret == {"actual result": "value"}
-
-    yield hub
-
-    # Teardown
-    ret = await hub.states.azurerm.resource.group.absent("name")
-    assert ret == {"actual result": "value"}
-
-
-@pytest.mark.asyncio
-async def test_present(hub):
-    ret = await hub.states.azurerm.resource.group.present("name")
-    assert ret == {"actual result": "value"}
-
-
-# /option 1
-
-# option 2
 @pytest.mark.asyncio
 async def test_absent_present(hub, subtests):
-    ret = await hub.states.azurerm.resource.group.absent("name")
+    ctx = {"acct": {}, "test": False}
+    ret = await hub.states.azurerm.resource.group.absent(ctx, "name")
     assert ret == {"actual result": "value"}
 
     with subtests.test(action="present"):
-        ret = await hub.states.azurerm.resource.group.present("name")
+        ret = await hub.states.azurerm.resource.group.present(ctx, "name")
         assert ret == {"actual result": "value"}
 
-    ret = await hub.states.azurerm.resource.group.absent("name")
+    ret = await hub.states.azurerm.resource.group.absent(ctx, "name")
     assert ret == {"actual result": "value"}
-# /option 2
