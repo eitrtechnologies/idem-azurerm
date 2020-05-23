1ff98c5c96043985c38e69c726d99ef22e9417ae
nicholasmhughes <nicholasmhughes@gmail.com>
2020-05-23 18:31:23 -0400

    added treqs to ensure proper ordering

--- idem_azurerm/states/azurerm/network/load_balancer.py
+++ idem_azurerm/states/azurerm/network/load_balancer.py
@@ -97,6 +97,8 @@ TREQ = {
         "require": [
             "states.azurerm.resource.group.present",
             "states.azurerm.network.public_ip_address.present",
+            "states.azurerm.network.virtual_network.present",
+            "states.azurerm.network.virtual_network.subnet_present",
         ]
     },
 }
