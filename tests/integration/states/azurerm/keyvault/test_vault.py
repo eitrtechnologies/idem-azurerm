e57d9024057a55f72c97972246696003c088c826
nicholasmhughes <nicholasmhughes@gmail.com>
2020-06-28 15:00:39 -0400

    use fixtures for keyvault name

--- tests/integration/states/azurerm/keyvault/test_vault.py
+++ tests/integration/states/azurerm/keyvault/test_vault.py
@@ -1,15 +1,19 @@
 import pytest
+import time
 
 
-# TODO: move this into a fixture
-KEYVAULT = "kv-idem-inttest"
-
-
-@pytest.mark.run(before="test_absent")
+@pytest.mark.second
 @pytest.mark.asyncio
-async def test_present(hub, ctx, resource_group, location):
+async def test_present(hub, ctx, resource_group, location, keyvault):
     tenant_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("tenant")
-    object_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    app_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    object_id = next(
+        iter(
+            await hub.exec.azurerm.graphrbac.service_principal.list(
+                ctx, sp_filter=f"appId eq '{app_id}'"
+            )
+        )
+    )
     sku = "standard"
     access_policies = [
         {
@@ -49,37 +53,46 @@ async def test_present(hub, ctx, resource_group, location):
         "changes": {
             "new": {
                 "access_policies": access_policies,
-                "enable_soft_delete": True,
+                "enable_soft_delete": False,
                 "location": location,
-                "name": KEYVAULT,
+                "name": keyvault,
                 "tenant_id": tenant_id,
                 "resource_group": resource_group,
                 "sku": "standard",
             },
             "old": {},
         },
-        "comment": f"Key Vault {KEYVAULT} has been created.",
-        "name": KEYVAULT,
+        "comment": f"Key Vault {keyvault} has been created.",
+        "name": keyvault,
         "result": True,
     }
     ret = await hub.states.azurerm.keyvault.vault.present(
         ctx,
-        KEYVAULT,
+        keyvault,
         resource_group,
         location,
         tenant_id,
         sku,
         access_policies=access_policies,
-        enable_soft_delete=True,
+        enable_soft_delete=False,
     )
+    # sleep because access policies need some time to take effect
+    time.sleep(5)
     assert ret == expected
 
 
 @pytest.mark.run(after="test_present", before="test_absent")
 @pytest.mark.asyncio
-async def test_changes(hub, ctx, resource_group, location, tags):
+async def test_changes(hub, ctx, resource_group, location, tags, keyvault):
     tenant_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("tenant")
-    object_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    app_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    object_id = next(
+        iter(
+            await hub.exec.azurerm.graphrbac.service_principal.list(
+                ctx, sp_filter=f"appId eq '{app_id}'"
+            )
+        )
+    )
     sku = "standard"
     access_policies = [
         {
@@ -116,28 +129,39 @@ async def test_changes(hub, ctx, resource_group, location, tags):
         }
     ]
     expected = {
-        "changes": {
-            "tags": {"new": tags,},
-            "access_policies": {"old": access_policies, "new": None},
-        },
-        "comment": f"Key Vault {KEYVAULT} has been updated.",
-        "name": KEYVAULT,
+        "changes": {"tags": {"new": tags,},},
+        "comment": f"Key Vault {keyvault} has been updated.",
+        "name": keyvault,
         "result": True,
     }
     ret = await hub.states.azurerm.keyvault.vault.present(
-        ctx, KEYVAULT, resource_group, location, tenant_id, sku, tags=tags
+        ctx,
+        keyvault,
+        resource_group,
+        location,
+        tenant_id,
+        sku,
+        access_policies=access_policies,
+        tags=tags,
     )
     assert ret == expected
 
 
-@pytest.mark.run(after="test_present")
+@pytest.mark.second_to_last
 @pytest.mark.asyncio
-async def test_absent(hub, ctx, resource_group, location, tags):
+async def test_absent(hub, ctx, resource_group, location, tags, keyvault):
     subscription_id = (
         hub.acct.PROFILES["azurerm"].get("default", {}).get("subscription_id")
     )
     tenant_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("tenant")
-    object_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    app_id = hub.acct.PROFILES["azurerm"].get("default", {}).get("client_id")
+    object_id = next(
+        iter(
+            await hub.exec.azurerm.graphrbac.service_principal.list(
+                ctx, sp_filter=f"appId eq '{app_id}'"
+            )
+        )
+    )
     access_policies = [
         {
             "tenant_id": tenant_id,
@@ -176,24 +200,24 @@ async def test_absent(hub, ctx, resource_group, location, tags):
         "changes": {
             "new": {},
             "old": {
-                "id": f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.KeyVault/vaults/{KEYVAULT}",
+                "id": f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.KeyVault/vaults/{keyvault}",
                 "location": location,
-                "name": KEYVAULT,
+                "name": keyvault,
                 "properties": {
-                    "access_policies": [],
-                    "enable_soft_delete": True,
+                    "access_policies": access_policies,
+                    "enable_soft_delete": False,
                     "enabled_for_deployment": False,
                     "sku": {"family": "A", "name": "standard"},
                     "tenant_id": f"{tenant_id}",
-                    "vault_uri": f"https://{KEYVAULT}.vault.azure.net/",
+                    "vault_uri": f"https://{keyvault}.vault.azure.net/",
                 },
                 "tags": tags,
                 "type": "Microsoft.KeyVault/vaults",
             },
         },
-        "comment": f"Key Vault {KEYVAULT} has been deleted.",
-        "name": KEYVAULT,
+        "comment": f"Key Vault {keyvault} has been deleted.",
+        "name": keyvault,
         "result": True,
     }
-    ret = await hub.states.azurerm.keyvault.vault.absent(ctx, KEYVAULT, resource_group)
+    ret = await hub.states.azurerm.keyvault.vault.absent(ctx, keyvault, resource_group)
     assert ret == expected
