91149f25337843ecb97496335faf09817591e0e3
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2020-07-13 13:34:03 -0400

    stashing progress on storage account tests

---
+++ tests/integration/states/azurerm/storage/test_account.py
@@ -0,0 +1,76 @@
+import pytest
+import time
+
+
+@pytest.mark.run(order=3)
+@pytest.mark.asyncio
+async def test_present(hub, ctx, resource_group, location, storage_account):
+    sku = 'Standard_LRS'
+    kind = 'StorageV2'
+    expected = {
+        "changes": {
+            "new": {
+                "name": storage_account,
+                "location": location,
+                "kind": kind,
+                "resource_group": resource_group,
+                "sku": sku,
+            },
+            "old": {},
+        },
+        "comment": f"Storage account {storage_account} has been created.",
+        "name": storage_account,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.storage.account.present(
+        ctx,
+        name=storage_account,
+        resource_group=resource_group,
+        location=location,
+        kind=kind,
+        sku=sku,
+    )
+    assert ret == expected
+
+
+@pytest.mark.run(after="test_present", before="test_absent")
+@pytest.mark.asyncio
+async def test_changes(hub, ctx, resource_group, location, storage_account):
+    sku = 'Standard_LRS'
+    kind = 'StorageV2'
+    expected = {
+        "changes": {"tags": {"new": tags,},},
+        "comment": f"Storage account {storage_account} has been updated.",
+        "name": storage_account,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.storage.account.present(
+        ctx,
+        name=storage_account,
+        resource_group=resource_group,
+        location=location,
+        sku=sku,
+        kind=kind,
+        tags=tags,
+    )
+    assert ret == expected
+
+
+@pytest.mark.run(order=)
+@pytest.mark.asyncio
+async def test_absent(hub, ctx, resource_group, storage_account):
+    expected = {
+        "changes": {
+            "new": {},
+            "old": {
+                "name": storage_account,
+            },
+        },
+        "comment": f"Storage account Vault {keyvault} has been deleted.",
+        "name": keyvault,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.storage.account.absent(ctx, storage_account, resource_group)
+    assert ret["changes"]["new"] == expected["changes"]["new"] &&
+           ret["changes"]["old"]["name"] == expected["changes"]["old"]["name"] &&
+           ret["result"] == expected["result"]
