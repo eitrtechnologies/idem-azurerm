0781c669628b5f5214fa753cf6690c3e8326853a
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2020-07-16 16:10:35 -0400

    Stashing progress

---
+++ tests/integration/states/azurerm/dns/test_record_set.py
@@ -0,0 +1,83 @@
+import pytest
+import string
+import random
+
+
+@pytest.fixture(scope="module")
+def record_type():
+    yield "A"
+
+
+@pytest.fixture(scope="module")
+def record_set():
+    yield "idem-record-set-" + "".join(
+        random.choice(string.ascii_lowercase + string.digits) for _ in range(8)
+    )
+
+
+@pytest.mark.run(order=4)
+@pytest.mark.asyncio
+async def test_present(hub, ctx, record_set, zone, resource_group, record_type):
+    expected = {
+        "changes": {
+            "new": {
+                "name": record_set,
+                "resource_group": resource_group,
+                "zone_name": zone,
+                "record_type": record_type,
+                "etag": None,
+                "metadata": None,
+                "ttl": None,
+            },
+            "old": {},
+        },
+        "comment": f"Record set {record_set} has been created.",
+        "name": record_set,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.dns.record_set.present(
+        ctx,
+        name=record_set,
+        resource_group=resource_group,
+        zone_name=zone,
+        record_type=record_type,
+    )
+    assert ret == expected
+
+
+@pytest.mark.run(after="test_present", before="test_absent")
+@pytest.mark.asyncio
+async def test_changes(hub, ctx, record_set, zone, resource_group, record_type):
+    metadata = {"zone": zone}
+    expected = {
+        "changes": {"metadata": {"new": metadata},},
+        "comment": f"Record set {record_set} has been updated.",
+        "name": record_set,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.dns.record_set.present(
+        ctx,
+        name=record_set,
+        resource_group=resource_group,
+        zone_name=zone,
+        record_type=record_type,
+        metadata=metadata,
+    )
+    assert ret == expected
+
+
+@pytest.mark.run(order=-4)
+@pytest.mark.asyncio
+async def test_absent(hub, ctx, record_set, zone, resource_group):
+    expected = {
+        "changes": {"new": {}, "old": {"name": record_set,},},
+        "comment": f"Record set {record_set} has been deleted.",
+        "name": record_set,
+        "result": True,
+    }
+    ret = await hub.states.azurerm.dns.record_set.absent(
+        ctx, name=record_set, zone_name=zone, resource_group=resource_group
+    )
+    assert ret["changes"]["new"] == expected["changes"]["new"]
+    assert ret["changes"]["old"]["name"] == expected["changes"]["old"]["name"]
+    assert ret["result"] == expected["result"]
