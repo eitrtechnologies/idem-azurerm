06aa46db4338f39792fe58dc1ee83d3593995d8b
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2020-08-18 18:21:35 -0400

    Public IP Prefix Module Addition

--- idem_azurerm/exec/azurerm/network/public_ip_prefix.py
+++ idem_azurerm/exec/azurerm/network/public_ip_prefix.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Azure Resource Manager (ARM) Network Public IP Address Execution Module
+Azure Resource Manager (ARM) Network Public IP Prefix Execution Module
 
-.. versionadded:: 1.0.0
-
-.. versionchanged:: 4.0.0
+.. versionadded:: 4.0.0
 
 :maintainer: <devops@eitr.tech>
 :configuration: This module requires Azure Resource Manager credentials to be passed as keyword arguments
@@ -53,93 +51,45 @@ __func_alias__ = {"list_": "list"}
 log = logging.getLogger(__name__)
 
 
-async def delete(hub, ctx, name, resource_group, **kwargs):
+async def create_or_update(
+    hub,
+    ctx,
+    name,
+    resource_group,
+    prefix_length,
+    sku="standard",
+    public_ip_address_version="IPv4",
+    zones=None,
+    **kwargs,
+):
     """
-    .. versionadded:: 1.0.0
+    .. versionadded:: 4.0.0
 
-    Delete a public IP address.
+    Creates or updates a static or dynamic public IP prefix.
 
-    :param name: The name of the public IP address to delete.
+    :param name: The name of the public IP prefix.
 
-    :param resource_group: The resource group name assigned to the public IP address.
+    :param resource_group: The resource group of the public IP prefix.
+
+    :param prefix_length: (Required) An integer representing the length of the Public IP Prefix. This value is immutable
+        once set. If the value of the ``public_ip_address_version`` parameter is "IPv4", then possible values include
+        28, 29, 30, 31. If the value of the ``public_ip_address_version`` parameter is "IPv6", then possible values
+        include 124, 125, 126, 127.
+
+    :param sku: (Optional) The name of a public IP prefix SKU. Possible values include: "standard". Defaults
+        to "standard".
+
+    :param public_ip_address_version: (Optional) The public IP address version. Possible values include: "IPv4" and
+        "IPv6". Defaults to "IPv4".
+
+    :param zones: (Optional) A list of availability zones that denotes where the IP allocated for the resource needs
+        to come from.
 
     CLI Example:
 
     .. code-block:: bash
 
-        azurerm.network.public_ip_address.delete test-pub-ip testgroup
-
-    """
-    result = False
-    netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
-
-    try:
-        pub_ip = netconn.public_ip_addresses.delete(
-            public_ip_address_name=name, resource_group_name=resource_group
-        )
-
-        pub_ip.wait()
-        result = True
-    except CloudError as exc:
-        await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
-
-    return result
-
-
-async def get(hub, ctx, name, resource_group, expand=None, **kwargs):
-    """
-    .. versionadded:: 1.0.0
-
-    .. versionchanged:: 4.0.0
-
-    Gets the specified public IP address in a specified resource group.
-
-    :param name: The name of the public IP address to query.
-
-    :param resource_group: The resource group of the public IP address.
-
-    :param expand: Expands referenced resources.
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        azurerm.network.public_ip_address.get test-pub-ip testgroup
-
-    """
-    result = {}
-    netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
-
-    try:
-        pub_ip = netconn.public_ip_addresses.get(
-            public_ip_address_name=name,
-            resource_group_name=resource_group,
-            expand=expand,
-        )
-
-        result = pub_ip.as_dict()
-    except CloudError as exc:
-        await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
-        result = {"error": str(exc)}
-
-    return result
-
-
-async def create_or_update(hub, ctx, name, resource_group, **kwargs):
-    """
-    .. versionadded:: 1.0.0
-
-    Creates or updates a static or dynamic public IP address.
-
-    :param name: The name of the public IP address to create.
-
-    :param resource_group: The resource group of the public IP address.
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        azurerm.network.public_ip_address.create_or_update test-ip-0 testgroup
+        azurerm.network.public_ip_prefix.create_or_update test_name test_group test_length
 
     """
     if "location" not in kwargs:
@@ -154,12 +104,21 @@ async def create_or_update(hub, ctx, name, resource_group, **kwargs):
             }
         kwargs["location"] = rg_props["location"]
 
+    if sku:
+        sku = {"name": sku.lower()}
+
     result = {}
     netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
 
     try:
-        pub_ip_model = await hub.exec.azurerm.utils.create_object_model(
-            "network", "PublicIPAddress", **kwargs
+        prefix_model = await hub.exec.azurerm.utils.create_object_model(
+            "network",
+            "PublicIPPrefix",
+            prefix_length=prefix_length,
+            sku=sku,
+            public_ip_address_version=public_ip_address_version,
+            zones=zones,
+            **kwargs,
         )
     except TypeError as exc:
         result = {
@@ -168,14 +127,14 @@ async def create_or_update(hub, ctx, name, resource_group, **kwargs):
         return result
 
     try:
-        ip = netconn.public_ip_addresses.create_or_update(
+        prefix = netconn.public_ip_prefixes.create_or_update(
             resource_group_name=resource_group,
-            public_ip_address_name=name,
-            parameters=pub_ip_model,
+            public_ip_prefix_name=name,
+            parameters=prefix_model,
         )
 
-        ip.wait()
-        result = ip.result().as_dict()
+        prefix.wait()
+        result = prefix.result().as_dict()
     except CloudError as exc:
         await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
         result = {"error": str(exc)}
@@ -187,13 +146,81 @@ async def create_or_update(hub, ctx, name, resource_group, **kwargs):
     return result
 
 
+async def delete(hub, ctx, name, resource_group, **kwargs):
+    """
+    .. versionadded:: 4.0.0
+
+    Deletes the specified public IP prefix.
+
+    :param name: The name of the public IP prefix to delete.
+
+    :param resource_group: The resource group of the public IP prefix.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        azurerm.network.public_ip_prefix.delete test_name test_group
+
+    """
+    result = False
+    netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
+
+    try:
+        prefix = netconn.public_ip_prefixes.delete(
+            public_ip_prefix_name=name, resource_group_name=resource_group
+        )
+
+        prefix.wait()
+        result = True
+    except CloudError as exc:
+        await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
+
+    return result
+
+
+async def get(hub, ctx, name, resource_group, expand=None, **kwargs):
+    """
+    .. versionadded:: 4.0.0
+
+    Gets the specified public IP prefix in a specified resource group.
+
+    :param name: The name of the public IP prefix to query.
+
+    :param resource_group: The resource group of the public IP prefix.
+
+    :param expand: Expands referenced resources.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        azurerm.network.public_ip_prefix.get test_name test_group
+
+    """
+    result = {}
+    netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
+
+    try:
+        prefix = netconn.public_ip_prefixes.get(
+            public_ip_prefix_name=name,
+            resource_group_name=resource_group,
+            expand=expand,
+        )
+
+        result = prefix.as_dict()
+    except CloudError as exc:
+        await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
+        result = {"error": str(exc)}
+
+    return result
+
+
 async def list_(hub, ctx, resource_group=None, **kwargs):
     """
-    .. versionadded:: 1.0.0
+    .. versionadded:: 4.0.0
 
-    .. versionchanged:: 4.0.0
-
-    Gets all the public IP addresses in a subscription.
+    Gets all the public IP prefixes in a subscription.
 
     :param resource_group: The name of the resource group to limit the results.
 
@@ -201,7 +228,7 @@ async def list_(hub, ctx, resource_group=None, **kwargs):
 
     .. code-block:: bash
 
-        azurerm.network.public_ip_address.list
+        azurerm.network.public_ip_prefix.list
 
     """
     result = {}
@@ -209,16 +236,16 @@ async def list_(hub, ctx, resource_group=None, **kwargs):
 
     try:
         if resource_group:
-            pub_ips = await hub.exec.azurerm.utils.paged_object_to_list(
-                netconn.public_ip_addresses.list(resource_group_name=resource_group)
+            prefixes = await hub.exec.azurerm.utils.paged_object_to_list(
+                netconn.public_ip_prefixes.list(resource_group_name=resource_group)
             )
         else:
-            pub_ips = await hub.exec.azurerm.utils.paged_object_to_list(
-                netconn.public_ip_addresses.list_all()
+            prefixes = await hub.exec.azurerm.utils.paged_object_to_list(
+                netconn.public_ip_prefixes.list_all()
             )
 
-        for ip in pub_ips:
-            result[ip["name"]] = ip
+        for prefix in prefixes:
+            result[prefix["name"]] = prefix
     except CloudError as exc:
         await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
         result = {"error": str(exc)}
@@ -230,11 +257,11 @@ async def update_tags(hub, ctx, name, resource_group, tags=None, **kwargs):
     """
     .. versionadded:: 4.0.0
 
-    Updates public IP address tags.
+    Updates public IP prefix tags.
 
-    :param name: The name of the public IP address.
+    :param name: The name of the public IP prefix.
 
-    :param resource_group: The resource group of the public IP address.
+    :param resource_group: The resource group of the public IP prefix.
 
     :param tags: The resource tags to update.
 
@@ -242,18 +269,18 @@ async def update_tags(hub, ctx, name, resource_group, tags=None, **kwargs):
 
     .. code-block:: bash
 
-        azurerm.network.public_ip_address.update_tags test_name test_group test_tags
+        azurerm.network.public_ip_prefix.update_tags test_name test_group test_tags
 
     """
+    result = {}
     netconn = await hub.exec.azurerm.utils.get_client(ctx, "network", **kwargs)
 
     try:
-        pub_ip = netconn.public_ip_addresses.update_tags(
-            public_ip_address_name=name, resource_group_name=resource_group, tags=tags
+        prefix = netconn.public_ip_prefixes.update_tags(
+            public_ip_prefix_name=name, resource_group_name=resource_group, tags=tags
         )
 
-        pub_ip.wait()
-        result = pub_ip.result().as_dict()
+        result = prefix.as_dict()
     except CloudError as exc:
         await hub.exec.azurerm.utils.log_cloud_error("network", str(exc), **kwargs)
         result = {"error": str(exc)}
---
+++ idem_azurerm/states/azurerm/network/public_ip_prefix.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+"""
+Azure Resource Manager (ARM) Network Public IP Prefix State Module
+
+.. versionadded:: 4.0.0
+
+:maintainer: <devops@eitr.tech>
+:configuration: This module requires Azure Resource Manager credentials to be passed via acct. Note that the
+    authentication parameters are case sensitive.
+
+    Required provider parameters:
+
+    if using username and password:
+      * ``subscription_id``
+      * ``username``
+      * ``password``
+
+    if using a service principal:
+      * ``subscription_id``
+      * ``tenant``
+      * ``client_id``
+      * ``secret``
+
+    Optional provider parameters:
+
+    **cloud_environment**: Used to point the cloud driver to different API endpoints, such as Azure GovCloud. Possible values:
+      * ``AZURE_PUBLIC_CLOUD`` (default)
+      * ``AZURE_CHINA_CLOUD``
+      * ``AZURE_US_GOV_CLOUD``
+      * ``AZURE_GERMAN_CLOUD``
+
+    Example acct setup for Azure Resource Manager authentication:
+
+    .. code-block:: yaml
+
+        azurerm:
+            default:
+                subscription_id: 3287abc8-f98a-c678-3bde-326766fd3617
+                tenant: ABCDEFAB-1234-ABCD-1234-ABCDEFABCDEF
+                client_id: ABCDEFAB-1234-ABCD-1234-ABCDEFABCDEF
+                secret: XXXXXXXXXXXXXXXXXXXXXXXX
+                cloud_environment: AZURE_PUBLIC_CLOUD
+            user_pass_auth:
+                subscription_id: 3287abc8-f98a-c678-3bde-326766fd3617
+                username: fletch
+                password: 123pass
+
+    The authentication parameters can also be passed as a dictionary of keyword arguments to the ``connection_auth``
+    parameter of each state, but this is not preferred and could be deprecated in the future.
+
+"""
+# Python libs
+from __future__ import absolute_import
+from dict_tools import differ
+import logging
+
+log = logging.getLogger(__name__)
+
+TREQ = {
+    "present": {"require": ["states.azurerm.resource.group.present"]},
+}
+
+
+async def present(
+    hub,
+    ctx,
+    name,
+    resource_group,
+    prefix_length=None,
+    sku="standard",
+    public_ip_address_version="IPv4",
+    zones=None,
+    tags=None,
+    connection_auth=None,
+    **kwargs,
+):
+    """
+    .. versionadded:: 4.0.0
+
+    Ensure a public IP prefix exists.
+
+    :param name: The name of the public IP prefix.
+
+    :param resource_group: The resource group of the public IP prefix.
+
+    :param prefix_length: (Required) An integer representing the length of the Public IP Prefix. This value is immutable
+        once set. If the value of the ``public_ip_address_version`` parameter is "IPv4", then possible values include
+        28, 29, 30, 31. If the value of the ``public_ip_address_version`` parameter is "IPv6", then possible values
+        include 124, 125, 126, 127.
+
+    :param sku: (Optional) The name of a public IP prefix SKU. Possible values include: "standard". Defaults
+        to "standard".
+
+    :param public_ip_address_version: (Optional) The public IP address version. Possible values include: "IPv4" and
+        "IPv6". Defaults to "IPv4".
+
+    :param zones: (Optional) A list of availability zones that denotes where the IP allocated for the resource needs
+        to come from.
+
+    :param tags: (Optional) A dictionary of strings can be passed as tag metadata to the public IP prefix object.
+
+    :param connection_auth: A dict with subscription and authentication parameters to be used in connecting to the
+        Azure Resource Manager API.
+
+    Example usage:
+
+    .. code-block:: yaml
+
+        Ensure public IP prefix exists:
+            azurerm.network.public_ip_prefix.present:
+                - name: test_prefix
+                - resource_group: test_group
+                - prefix_length: 28 
+                - sku: "standard"
+                - public_ip_version: "IPv4"
+                - tags:
+                    contact_name: Elmer Fudd Gantry
+                - connection_auth: {{ profile }}
+
+    """
+    ret = {"name": name, "result": False, "comment": "", "changes": {}}
+    action = "create"
+
+    if not isinstance(connection_auth, dict):
+        if ctx["acct"]:
+            connection_auth = ctx["acct"]
+        else:
+            ret[
+                "comment"
+            ] = "Connection information must be specified via acct or connection_auth dictionary!"
+            return ret
+
+    prefix = await hub.exec.azurerm.network.public_ip_prefix.get(
+        ctx, name, resource_group, azurerm_log_level="info", **connection_auth
+    )
+
+    if "error" not in prefix:
+        action = "update"
+
+        # tag changes
+        tag_changes = differ.deep_diff(prefix.get("tags", {}), tags or {})
+        if tag_changes:
+            ret["changes"]["tags"] = tag_changes
+
+        # public_ip_address_version changes
+        if public_ip_address_version:
+            if (
+                public_ip_address_version.lower()
+                != prefix.get("public_ip_address_version", "").lower()
+            ):
+                ret["changes"]["public_ip_address_version"] = {
+                    "old": prefix.get("public_ip_address_version"),
+                    "new": public_ip_address_version,
+                }
+
+        # zones changes
+        if zones is not None:
+            if zones.sort() != prefix.get("zones").sort():
+                ret["changes"]["zones"] = {"old": prefix.get("zones"), "new": zones}
+
+        if not ret["changes"]:
+            ret["result"] = True
+            ret["comment"] = "Public IP prefix {0} is already present.".format(name)
+            return ret
+
+        if ctx["test"]:
+            ret["result"] = None
+            ret["comment"] = "Public IP prefix {0} would be updated.".format(name)
+            return ret
+
+    else:
+        ret["changes"] = {
+            "old": {},
+            "new": {
+                "name": name,
+                "resource_group": resource_group,
+                "tags": tags,
+                "prefix_length": prefix_length,
+                "sku": {"name": sku},
+                "public_ip_address_version": public_ip_address_version,
+                "zones": (zones or []),
+            },
+        }
+
+    if ctx["test"]:
+        ret["comment"] = "Public IP prefix {0} would be created.".format(name)
+        ret["result"] = None
+        return ret
+
+    prefix_kwargs = kwargs.copy()
+    prefix_kwargs.update(connection_auth)
+
+    if action == "create" or len(ret["changes"]) > 1 or not tag_changes:
+        prefix = await hub.exec.azurerm.network.public_ip_prefix.create_or_update(
+            ctx=ctx,
+            name=name,
+            resource_group=resource_group,
+            prefix_length=prefix_length,
+            sku=sku,
+            tags=tags,
+            public_ip_address_version=public_ip_address_version,
+            zones=zones,
+            **prefix_kwargs,
+        )
+
+    # no idea why create_or_update doesn't work for tags
+    if action == "update" and tag_changes:
+        prefix = await hub.exec.azurerm.network.public_ip_prefix.update_tags(
+            ctx, name=name, resource_group=resource_group, tags=tags, **prefix_kwargs,
+        )
+
+    if "error" not in prefix:
+        ret["result"] = True
+        ret["comment"] = f"Public IP prefix {name} has been {action}d."
+        return ret
+
+    ret["comment"] = "Failed to {0} public IP prefix {1}! ({2})".format(
+        action, name, prefix.get("error")
+    )
+    if not ret["result"]:
+        ret["changes"] = {}
+    return ret
+
+
+async def absent(hub, ctx, name, resource_group, connection_auth=None, **kwargs):
+    """
+    .. versionadded:: 4.0.0
+
+    Ensure a public IP prefix does not exist in the resource group.
+
+    :param name: The name of the public IP prefix.
+
+    :param resource_group: The resource group assigned to the public IP prefix.
+
+    :param connection_auth: A dict with subscription and authentication parameters to be used in connecting to the
+        Azure Resource Manager API.
+
+    """
+    ret = {"name": name, "result": False, "comment": "", "changes": {}}
+
+    if not isinstance(connection_auth, dict):
+        if ctx["acct"]:
+            connection_auth = ctx["acct"]
+        else:
+            ret[
+                "comment"
+            ] = "Connection information must be specified via acct or connection_auth dictionary!"
+            return ret
+
+    prefix = await hub.exec.azurerm.network.public_ip_prefix.get(
+        ctx, name, resource_group, azurerm_log_level="info", **connection_auth
+    )
+
+    if "error" in prefix:
+        ret["result"] = True
+        ret["comment"] = "Public IP prefix {0} was not found.".format(name)
+        return ret
+
+    if ctx["test"]:
+        ret["comment"] = "Public IP prefix {0} would be deleted.".format(name)
+        ret["result"] = None
+        ret["changes"] = {
+            "old": prefix,
+            "new": {},
+        }
+        return ret
+
+    deleted = await hub.exec.azurerm.network.public_ip_prefix.delete(
+        ctx, name, resource_group, **connection_auth
+    )
+
+    if deleted:
+        ret["result"] = True
+        ret["comment"] = "Public IP prefix {0} has been deleted.".format(name)
+        ret["changes"] = {"old": prefix, "new": {}}
+        return ret
+
+    ret["comment"] = "Failed to delete public IP prefix {0}!".format(name)
+    return ret
