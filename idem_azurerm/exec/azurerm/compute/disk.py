abb19d8037c84ce391c311273ac0b379699501ee
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2020-08-29 10:36:21 -0400

    Began Compute bump

--- idem_azurerm/exec/azurerm/compute/disk.py
+++ idem_azurerm/exec/azurerm/compute/disk.py
@@ -4,6 +4,8 @@ Azure Resource Manager (ARM) Compute Disk Execution Module
 
 .. versionadded:: 1.0.0
 
+.. versiochanged:: 4.0.0
+
 :maintainer: <devops@eitr.tech>
 :configuration: This module requires Azure Resource Manager credentials to be passed as keyword arguments
     to every function or via acct in order to work properly.
@@ -31,7 +33,6 @@ Azure Resource Manager (ARM) Compute Disk Execution Module
       * ``AZURE_GERMAN_CLOUD``
 
 """
-
 # Python libs
 from __future__ import absolute_import
 import logging
@@ -42,13 +43,44 @@ try:
     import azure.mgmt.compute.models  # pylint: disable=unused-import
     from msrest.exceptions import SerializationError
     from msrestazure.azure_exceptions import CloudError
-    from msrestazure.tools import is_valid_resource_id
 
     HAS_LIBS = True
 except ImportError:
     pass
 
+__func_alias__ = {"list_": "list"}
+
 log = logging.getLogger(__name__)
+
+
+async def get(hub, ctx, name, resource_group, **kwargs):
+    """
+    .. versionadded:: 4.0.0
+
+    Gets information about a disk.
+
+    :param name: The disk to query.
+
+    :param resource_group: The resource group name assigned to the disk.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        azurerm.compute.disk.get testdisk testgroup
+
+    """
+    result = {}
+    compconn = await hub.exec.azurerm.utils.get_client(ctx, "compute", **kwargs)
+
+    try:
+
+        disk = compconn.disks.get(resource_group_name=resource_group, disk_name=name)
+        result = disk.as_dict()
+    except CloudError as exc:
+        await hub.exec.azurerm.utils.log_cloud_error("compute", str(exc), **kwargs)
+
+    return result
 
 
 async def delete(hub, ctx, name, resource_group, **kwargs):
@@ -70,11 +102,49 @@ async def delete(hub, ctx, name, resource_group, **kwargs):
     """
     result = False
     compconn = await hub.exec.azurerm.utils.get_client(ctx, "compute", **kwargs)
-    try:
-        compconn.disks.delete(resource_group_name=resource_group, disk_name=name)
-        result = True
 
+    try:
+
+        disk = compconn.disks.delete(resource_group_name=resource_group, disk_name=name)
+        result = True
     except CloudError as exc:
         await hub.exec.azurerm.utils.log_cloud_error("compute", str(exc), **kwargs)
+
+    return result
+
+
+async def list_(hub, ctx, resource_group=None, **kwargs):
+    """
+    .. versionadded:: 4.0.0
+
+    Lists all the disks under a subscription.
+
+    :param resource_group: The name of the resource group to limit the results.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        azurerm.compute.disk.list
+
+    """
+    result = {}
+    compconn = await hub.exec.azurerm.utils.get_client(ctx, "compute", **kwargs)
+
+    try:
+        if resource_group:
+            disks = await hub.exec.azurerm.utils.paged_object_to_list(
+                compconn.disks.list_by_resource_group(resource_group_name=resource_group)
+            )
+        else:
+            disks = await hub.exec.azurerm.utils.paged_object_to_list(
+                compconn.disks.list()
+            )
+
+        for disk in disks:
+            result[disk["name"]] = disk
+    except CloudError as exc:
+        await hub.exec.azurerm.utils.log_cloud_error("compute", str(exc), **kwargs)
+        result = {"error": str(exc)}
 
     return result
